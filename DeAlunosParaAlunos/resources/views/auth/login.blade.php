72128551ab82c2bfac574228e20b614c7425d005
HugOtavioS <hugotavio2005@gmail.com>
2025-02-03 15:15:22 -0300

    Update login.blade.php
    Modificado apelida do rota corretamente;
    
    Adicionado proteção contra CSRF (O Laravel não permite enviar requisições POST sem essa proteção);

--- DeAlunosParaAlunos/resources/views/auth/login.blade.php
+++ DeAlunosParaAlunos/resources/views/auth/login.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="form-section w-full h-full flex justify-center items-center">
         <div class="form-container lg:w-1/3 mx-5 rounded-lg py-2 px-5 shadow-sm shadow-slate-400 bg-zinc-600/75 text-white text-xl">
-            <form action="{{route('login')}}" class="h-full" method="POST">
+            <form action="{{route('login.post')}}" class="h-full" method="post">
+                @csrf
                 <div class="form-body">
                     <div class="w-full justify-center items-center flex">
                         <img class="w-1/2" src="{{ asset('img/DAPA.png') }}" alt="">
